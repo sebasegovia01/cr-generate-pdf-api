f1789d430d51604a4636829b1008fd8ce3131694
Jaime Quiñones <jaime.quinones.t@gmail.com>
2022-10-29 18:51:12 -0300

    Aply hot reload configuration and play with code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CreateController } from './controllers/create/create.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CreateController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
