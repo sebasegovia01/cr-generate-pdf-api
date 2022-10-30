0c0848f3a8235b996f38987dd15b431eed890880
Jaime Quiñones <jaime.quinones.t@gmail.com>
2022-10-30 02:17:52 -0300

    Test 01 complete proceses

--- src/resources/generate/generate.controller.ts
+++ src/resources/generate/generate.controller.ts
@@ -1,7 +1,24 @@
 import { GeneratePdfDto } from '@dtos/generate-pdf.dto';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { GenerateService } from './generate.service';
 import { Storage } from '@google-cloud/storage';
+import Libre from 'libreoffice-convert';
+import { LoggingWinston } from '@google-cloud/logging-winston';
+import winston from 'winston';
+
+// Create logging
+const loggingWinston = new LoggingWinston();
+
+// Create a Winston logger that streams to Cloud Logging
+// Logs will be written to: "projects/YOUR_PROJECT_ID/logs/winston_log"
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [
+    new winston.transports.Console(),
+    // Add Cloud Logging
+    loggingWinston,
+  ],
+});
 
 @Controller('generate')
 export class GenerateController {
@@ -11,22 +28,47 @@ export class GenerateController {
   @Post()
   async generate(@Body() body: GeneratePdfDto): Promise<any> {
 
-    console.log(body);
-
+    const { template } = body;
+    const bucketName = 'nani-food-dev-pdf-templates';
+    const processedBucketName = 'nani-food-dev-pdf-processed';
     const storage = new Storage();
+    try {
+      const htmlTemplate = await (await storage.bucket(bucketName).file(template).download()).toString();
+      logger.info({ step: 'Getting content', content: htmlTemplate });
+      const bufferContents = Buffer.from(htmlTemplate);
+      logger.info({ step: 'Generate buffer.from', content: bufferContents });
+      await Libre.convert(bufferContents, '/tmp-pdf/generate-pdf.pdf', undefined, async (err, data) => {
+        logger.info({ step: 'Create PDF', content: bufferContents });
 
+        logger.info(err);
+        logger.info(data);
+
+        const newFile = await storage.bucket(processedBucketName).file('generate-pdf.pdf').save(data);
+        logger.info(newFile);
+        logger.info('PDF Created successfully');
+      });
+
+    } catch (error) {
+      console.log('Error trying generate pdf');
+      console.log(error);
+    }
+
+    const response = await await this.generateService.generatePDF();
+    return response;
+  }
+
+  @Get('list')
+  listfiles(): Promise<any> {
+    const storage = new Storage();
+    const filesList = [];
     async function listFiles() {
       // Lists files in the bucket
       const [files] = await storage.bucket('nani-food-dev-pdf-templates').getFiles();
-
-      console.log('Files:');
       files.forEach(file => {
-        console.log(file.name);
+        filesList.push(file.name);
       });
     }
     listFiles().catch(console.error);
-    
-    const response = await await this.generateService.generatePDF();
-    return response;
+    return Promise.resolve(filesList);
   }
 }
