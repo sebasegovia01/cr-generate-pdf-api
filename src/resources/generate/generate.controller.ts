3c1b77a0f3b666cf41a3c50bb37e050af858e779
Jaime Quiñones <jaime.quinones.t@gmail.com>
2022-10-30 01:21:27 -0300

    Listando archivos

--- src/resources/generate/generate.controller.ts
+++ src/resources/generate/generate.controller.ts
@@ -1,6 +1,7 @@
 import { GeneratePdfDto } from '@dtos/generate-pdf.dto';
 import { Body, Controller, Post } from '@nestjs/common';
 import { GenerateService } from './generate.service';
+import { Storage } from '@google-cloud/storage';
 
 @Controller('generate')
 export class GenerateController {
@@ -9,7 +10,22 @@ export class GenerateController {
 
   @Post()
   async generate(@Body() body: GeneratePdfDto): Promise<any> {
+
     console.log(body);
+
+    const storage = new Storage();
+
+    async function listFiles() {
+      // Lists files in the bucket
+      const [files] = await storage.bucket('nani-food-dev-pdf-templates').getFiles();
+
+      console.log('Files:');
+      files.forEach(file => {
+        console.log(file.name);
+      });
+    }
+    listFiles().catch(console.error);
+    
     const response = await await this.generateService.generatePDF();
     return response;
   }
