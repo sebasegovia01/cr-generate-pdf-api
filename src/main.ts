207ad21aea227d78f97259b176fdf28e96b4c605
Sebastián Segovia S <ssegovias@bancoripley.com>
2024-02-23 13:10:23 -0300

    [fix]: added body parser

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(bodyParser.json({ limit: '50mb' })); // for JSON request
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); // for URL-encoded request
+
   await app.listen(parseInt(process.env.PORT) || 3000);
 
   if (module.hot) {
